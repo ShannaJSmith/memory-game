c4922e8408211e3c060f6dbe863ed6d3cf101eda
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-02 18:56:32 -0500

    create state and function for shuffling cards

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
+const images = [
+  { src: 'images/brook.png' },
+  { src: 'images/chopper.png' },
+  { src: 'images/franky.png' },
+  { src: 'images/luffy.png' },
+  { src: 'images/nami.png' },
+  { src: 'images/robin.png' },
+  { src: 'images/sanji.png' },
+  { src: 'images/ussop.png' },
+  { src: 'images/zoro.png' },
+  { src: 'images/whitebeard.png' },
+  { src: 'images/mihawk.png' },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+
+  // to shuffle cards:
+  const shuffleCards = () => {
+    const shuffledCards = [...images, ...images]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+  };
+
   return (
     <div className="App">
       <h1>Jolly Roger Memory Game</h1>
