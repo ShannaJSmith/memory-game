af578cbb46f3c8b20969fef52152a85ccd514624
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-02 18:21:07 -0500

    import google font and set base styles in app.css and index.css

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Memory Game</div>;
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <button>New Game</button>
+    </div>
+  );
 }
 
 export default App;
